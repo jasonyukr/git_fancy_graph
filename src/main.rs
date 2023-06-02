19e16dbf94c0f0ca704d73d92b374c4b3cfebacd
Jason (Jin-Hwan) Yu <jin-hwan.yu@oracle.com>
2023-06-02 16:29:55 +0900

    Preserve unicode characters

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
 use std::io::{self, BufRead};
 
-const STRIP_COLOR: bool = false;
-
-enum State {
-    Normal,
-    Escape,
-    Csi,
-}
-
 fn print_conv(graph_end: &mut bool, ch: char) {
     if *graph_end == false {
         *graph_end = ch.is_digit(16)
@@ -36,39 +28,9 @@ fn main() {
             Ok(data) => line_data = data,
             Err(_) => continue
         }
-        let mut state = State::Normal;
         let mut graph_end = false;
-        for b in line_data.bytes() {
-            match &state {
-                State::Normal => {
-                    if b == 0x1B { // ESC
-                        state = State::Escape;
-                        if !STRIP_COLOR {
-                            print!("{}", b as char);
-                        }
-                    } else {
-                        print_conv(&mut graph_end, b as char);
-                    }
-                },
-                State::Escape => {
-                    if !STRIP_COLOR {
-                        print!("{}", b as char);
-                    }
-                    if b == 0x5B { // [
-                        state = State::Csi;
-                    } else {
-                        state = State::Normal;
-                    }
-                },
-                State::Csi => {
-                    if !STRIP_COLOR {
-                        print!("{}", b as char);
-                    }
-                    if b >= 0x40 && b < 0x80 {
-                        state = State::Normal;
-                    }
-                },
-            }
+        for ch in line_data.chars() {
+            print_conv(&mut graph_end, ch);
         }
         println!();
     }
