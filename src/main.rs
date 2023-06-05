16535d2ff38ae4b5ca7ee8a3d89ad433d9dc419b
Jason (Jin-Hwan) Yu <jin-hwan.yu@oracle.com>
2023-06-05 14:00:10 +0900

    Resurrect state code and use chars type for correct unicode handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use std::io::{self, BufRead};
 
+const STRIP_COLOR: bool = false;
+
+enum State {
+    Normal,
+    Escape,
+    Csi,
+}
+
 fn print_conv(graph_end: &mut bool, ch: char) {
     if *graph_end == false {
         *graph_end = ch.is_digit(16)
@@ -28,9 +36,39 @@ fn main() {
             Ok(data) => line_data = data,
             Err(_) => continue
         }
+        let mut state = State::Normal;
         let mut graph_end = false;
-        for ch in line_data.chars() {
-            print_conv(&mut graph_end, ch);
+        for c in line_data.chars() {
+            match &state {
+                State::Normal => {
+                    if c == 0x1B as char { // ESC
+                        state = State::Escape;
+                        if !STRIP_COLOR {
+                            print!("{}", c);
+                        }
+                    } else {
+                        print_conv(&mut graph_end, c);
+                    }
+                },
+                State::Escape => {
+                    if !STRIP_COLOR {
+                        print!("{}", c);
+                    }
+                    if c == 0x5B as char { // [
+                        state = State::Csi;
+                    } else {
+                        state = State::Normal;
+                    }
+                },
+                State::Csi => {
+                    if !STRIP_COLOR {
+                        print!("{}", c);
+                    }
+                    if c >= 0x40 as char && c < 0x80 as char {
+                        state = State::Normal;
+                    }
+                },
+            }
         }
         println!();
     }
